9bcc884062e39106be8b8f7f360aebe3036ce29f
Anubhav Singh <80422816+fakeanubhav@users.noreply.github.com>
2021-11-29 14:08:53 +0530

    added something

--- app.js
+++ app.js
@@ -4,18 +4,8 @@ const mongoose = require('mongoose');
 const User = require('./models/user');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.static("public"));
 
-app.use(session({
-    secret: 'notasecret',
-    resave: true,
-    saveUninitialized: true
-}));
+
 mongoose.connect('mongodb://localhost:27017/blood-donation-db')
     .then(() => {
         console.log("Mongoose Connection Open");
@@ -24,6 +14,26 @@ mongoose.connect('mongodb://localhost:27017/blood-donation-db')
         console.log("Error");
         console.log(err);
     })
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+// const sessionOptions = ;
+app.use(session({
+    // cookie: { maxAge: 60000 },
+    secret: 'notasecret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.session.user_id;
+    next();
+});
+
 
 const requireLogin = (req, res, next) => {
     if (!req.session.user_id) {
@@ -31,7 +41,7 @@ const requireLogin = (req, res, next) => {
     }
     next();
 }
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
     res.render('home');
 })
 
@@ -46,8 +56,8 @@ app.post('/login', async (req, res) => {
     } = req.body;
     const foundUser = await User.findAndValidate(username, password)
     if (foundUser) {
+
         req.session.user_id = foundUser._id;
-        // res.redirect('/secret');
         res.redirect('/');
     } else {
         res.redirect('/login');
@@ -55,7 +65,7 @@ app.post('/login', async (req, res) => {
 })
 app.post('/logout', (req, res) => {
     req.session.destroy();
-    res.redirect('/login');
+    res.redirect('/');
 })
 
 app.post('/emergency', requireLogin, (req, res) => {
@@ -68,12 +78,16 @@ app.post('/emergency', requireLogin, (req, res) => {
     // add sending email feature
     res.send('Email is Sent to Admin and Other Doners with required Blood Group');
 })
-app.post('/requestblood', requireLogin, (req, res)=>{
-    const {bloodGroup, reason} = req.body;
+app.post('/requestblood', requireLogin, (req, res) => {
+    const {
+        bloodGroup,
+        reason
+    } = req.body;
+
     console.log(bloodGroup);
     console.log(reason);
     // add these things to other collection to show admin
-    res.send("Request Sent Admin will Contact Soon on You Email");
+    // res.send("Request Sent Admin will Contact Soon on You Email");
 })
 
 app.get('/register', (req, res) => {
@@ -104,8 +118,10 @@ app.get('/admin', requireLogin, (req, res) => {
     })
 })
 
-app.post('/doner', requireLogin, (req, res)=>{
-    const {numberofunits} = req.body;
+app.post('/doner', requireLogin, (req, res) => {
+    const {
+        numberofunits
+    } = req.body;
     const uid = req.session.user_id;
     //update in db
     res.send("You can go to nearest camp to donate the blood");
@@ -114,7 +130,7 @@ app.post('/doner', requireLogin, (req, res)=>{
 app.get('/patient', requireLogin, (req, res) => {
     res.render('patient');
 })
-app.get('/blood', (req, res)=>{
+app.get('/blood', (req, res) => {
     res.render('blood');
 })
 app.post('/register', async (req, res) => {
@@ -129,7 +145,7 @@ app.post('/register', async (req, res) => {
     } = req.body;
     const isAdmin = false;
     const donations = 0;
-    if(isDoner==undefined){
+    if (isDoner == undefined) {
         isDoner = false;
     }
     const user = new User({
