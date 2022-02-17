f28b1fd8604d8e7e50fb7d8bec3132ebcc94c72e
Anubhav Singh <singhanubhav58@gmail.com>
2022-02-17 10:12:40 +0530

    minor indentation changes

--- bloodCompat.js
+++ bloodCompat.js
@@ -1,34 +1,34 @@
 function bloodCompatibilityChecker(Donor, Recipient) {
-    if (Recipient == "A+") {
-        if (Donor == "A+" || Donor == "A-" || Donor == "O+" || Donor == "O-") {
+    if (Recipient == 'A+') {
+        if (Donor == 'A+' || Donor == 'A-' || Donor == 'O+' || Donor == 'O-') {
             return true;
         }
-    } else if (Recipient == "A-") {
-        if (Donor == "A-" || Donor == "O-") {
+    } else if (Recipient == 'A-') {
+        if (Donor == 'A-' || Donor == 'O-') {
             return true;
         }
-    } else if (Recipient == "B+") {
-        if (Donor == "B+" || Donor == "B-" || Donor == "O-" || Donor == "O+") {
+    } else if (Recipient == 'B+') {
+        if (Donor == 'B+' || Donor == 'B-' || Donor == 'O-' || Donor == 'O+') {
             return true;
         }
-    } else if (Recipient == "B-") {
-        if (Donor == "B-" || Donor == "O-") {
+    } else if (Recipient == 'B-') {
+        if (Donor == 'B-' || Donor == 'O-') {
             return true;
         }
-    } else if (Recipient == "AB+") {
-        if (Donor == "A+" || Donor == "A-" || Donor == "B+" || Donor == "B-" || Donor == "AB+" || Donor == "AB-" || Donor == "O+" || Donor == "O-") {
+    } else if (Recipient == 'AB+') {
+        if (Donor == 'A+' || Donor == 'A-' || Donor == 'B+' || Donor == 'B-' || Donor == 'AB+' || Donor == 'AB-' || Donor == 'O+' || Donor == 'O-') {
             return true;
         }
-    } else if (Recipient == "AB-") {
-        if (Donor == "AB-" || Donor == "A-" || Donor == "B-" || Donor == "O-") {
+    } else if (Recipient == 'AB-') {
+        if (Donor == 'AB-' || Donor == 'A-' || Donor == 'B-' || Donor == 'O-') {
             return true;
         }
-    } else if (Recipient == "O+") {
-        if (Donor == "O+" || Donor == "O-") {
+    } else if (Recipient == 'O+') {
+        if (Donor == 'O+' || Donor == 'O-') {
             return true;
         }
-    } else if (Recipient == "O-") {
-        if (Donor == "O-") {
+    } else if (Recipient == 'O-') {
+        if (Donor == 'O-') {
             return true;
         }
     }
